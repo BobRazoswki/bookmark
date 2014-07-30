d23148bc74ee2c336442aacf0fc64efe512c0896
BobRazoswki <bobongithub@gmail.com>
2014-07-30 18:50:38 +0100

    ilhb

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,5 +4,5 @@ require_relative 'models/link' # this needs to be done after datamapper is initi
 require_relative 'models/tag'
 require_relative 'models/user'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 DataMapper.finalize
